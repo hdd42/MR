581ef393cebdb2db4a45008f85621ff58aaf3b73
HasanKeyf <donmez81@gmail.com>
2015-04-15 04:09:59 -0400

    bug fix

--- Scripts/Angular/controller.js
+++ Scripts/Angular/controller.js
@@ -12,24 +12,25 @@
         $scope.allAdds = [];
         $scope.getTopFives = [];
         $scope.getOnTheCover50 = [];
-
+        $scope.setItemsPerPage = 10;
 
         $scope.title = "Listed all adds";
 
         mrApi.getAllAds().then(function(adds) {
             $scope.adds = adds;
             $scope.loading = false;
-        }).then(function () {
-            $scope.allAdds = $scope.adds;
-            mrApi.getTopFives().then(function(top5) {
-                $scope.getTopFives = top5;
-            });
         }).then(function() {
-            mrApi.getOnTheCover50().then(function(cover50) {
-                $scope.getOnTheCover50 = cover50;
-            });
+            $scope.allAdds = $scope.adds;
+
         });
 
+        mrApi.getTopFives().then(function (top5) {
+            $scope.getTopFives = top5;
+        });
+
+        mrApi.getOnTheCover50().then(function (cover50) {
+            $scope.getOnTheCover50 = cover50;
+        });
 
         $scope.$watch("extraFilters", function (newValue) {
 
